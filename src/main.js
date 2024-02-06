3040c06ba8e7b06caf9f93c2884e9086ef4a3db0
Ivanka Zlateva <it.vania.9@gmail.com>
2024-02-06 09:18:16 +0200

    Add bootstrap and sass files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
