235c6a211e36f27e89d8f55cf05ecbf28a7176ea
Ivanka Zlateva <it.vania.9@gmail.com>
2024-02-06 17:13:53 +0200

    Migrate GetCategory and JokesView

---
+++ src/components/GetCategory.vue
@@ -0,0 +1,39 @@
+<template>
+  <select class="dropdown-content" v-model="category" @click="selectCategory(category)">
+    <option value="">Choose Category</option>
+    <option v-for="(category, index) in categories" :key="index" :value="category">
+      {{ category ? category.slice(0, 1).toUpperCase() + category.substring(1) : '' }}
+    </option>
+  </select>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const emit = defineEmits(['handle-category-change'])
+const categories = ref([])
+
+const errorMsg = ref('')
+const category = ref('')
+
+async function getCategory() {
+  try {
+    const response = await axios.get('https://api.chucknorris.io/jokes/categories')
+    categories.value = response.data
+    console.log('CHeck this:', categories.value)
+  } catch (error) {
+    errorMsg.value = 'Error retrieving category!'
+  }
+}
+
+function selectCategory(selectedCategory) {
+  category.value = selectedCategory;
+  console.log('category-value', category.value);
+  emit('handle-category-change', category.value);
+}
+
+onMounted(() => {
+  getCategory()
+})
+</script>
--- src/views/JokesView.vue
+++ src/views/JokesView.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>Jokes</div>
+  <div class="joke-wrapper">
+    <GetCategory class="button-get-category me-5" @handle-category-change="handleCategoryChange" />
+    <GetJoke @joke-object="getJokeObj" @error-message="getErrorMessage" :category="category" />
+<!--    <p class="text-white">{{result.resultFromResponse.value}}</p>-->
+    <CardContainer
+      :joke="result.resultFromResponse.value"
+      :id="result.resultFromResponse.id"
+      :result="result.resultFromResponse"
+      :error-message="errorMsg"
+    />
+
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {reactive, ref} from 'vue'
+import GetJoke from '@/components/GetJoke.vue'
+import CardContainer from '@/components/CardContainer.vue'
+import GetCategory from '@/components/GetCategory.vue'
+
+const category = ref('');
+const result = reactive({
+  resultFromResponse: {}
+});
+const errorMsg = ref('');
+
+const handleCategoryChange = (selectedCategory) => {
+  category.value = selectedCategory;
+}
+
+const getJokeObj = (response) => {
+  result.resultFromResponse = {...response};
+ return result.resultFromResponse;
+}
+
+const getErrorMessage = (message) => {
+  errorMsg.value = message;
+}
+</script>
