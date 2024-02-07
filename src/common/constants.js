511887a34ae59fde1cc42ebb049241c3cded22ab
Ivanka Zlateva <it.vania.9@gmail.com>
2024-02-07 16:45:41 +0200

    Add constants

---
+++ src/common/constants.js
@@ -0,0 +1,10 @@
+export const LOCAL_STORAGE_GET_DATA = JSON.parse(localStorage.getItem('favorites'));
+
+export const MESSAGE_FIRST = 'Welcome to';
+
+export const MESSAGE_SECOND = 'Chuck Norris Jokes Page';
+
+export const URL_GET_JOKE = `https://api.chucknorris.io/jokes/random`;
+
+export const URL_GET_CATEGORY = 'https://api.chucknorris.io/jokes/categories';
+
