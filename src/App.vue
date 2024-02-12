d1ac864e9d24121a1d3b1a306a102b0ad2434e80
Ivanka Zlateva <it.vania.9@gmail.com>
2024-02-12 10:46:25 +0200

    Add main to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
+<template>
+  <NavigationBar />
+  <main>
+    <RouterView />
+  </main>
+</template>
 <script setup>
 import { RouterView } from 'vue-router'
 import NavigationBar from '@/components/NavigationBar.vue'
 </script>
 
-<template>
-  <NavigationBar />
-  <RouterView />
-</template>
 <style lang="sass">
 @import "./assets/style/style"
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-  <main class="home-page d-flex flex-row justify-content-center align-items-center gap-5">
+  <div class="home-page d-flex flex-row justify-content-center align-items-center gap-5">
     <GreetMessage :msg-first="MESSAGE_FIRST" :msg-second="MESSAGE_SECOND" />
     <div class="image-container">
       <img src="../assets/image-chuck.png" alt="Chuck Norris" width="420px" />
       <RouterLink to="/jokes" class="button-router"> Get Jokes </RouterLink>
     </div>
-  </main>
+  </div>
 </template>
 
 <script setup>
