3340ced04fcd2d95a77b7cd4e8da6f49f881e77b
Ivanka Zlateva <it.vania.9@gmail.com>
2024-02-12 11:42:06 +0200

    Add router lin to button for navigation to home view

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="d-flex flex-column justify-content-sm-center align-items-center">
   <h3 class="text-white">Error 404... try again</h3>
-  <button class="button-get-category mb-3">Back to Home</button>
+  <RouterLink to="/home" class="button-get-category mb-3">Back to Home</RouterLink>
   <img src="../assets/error.gif" alt="Where is it?" width="498" />
 </div>
 </template>
